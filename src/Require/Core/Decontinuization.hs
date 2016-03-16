652207f1826e49db4aab43f2272206df8361571a
Darryl McAdams <psygnisfive@yahoo.com>
2016-03-16 19:19:18 -0400

    fix decontinuization scope bug
    previous version uses haskell functions to make continuations, leading
    to nasty capture issues. this version uses native scopes instead to
    avoid this problem.

--- src/Require/Core/Decontinuization.hs
+++ src/Require/Core/Decontinuization.hs
@@ -11,9 +11,9 @@
 module Require.Core.Decontinuization where
 
 import Utils.ABT hiding (shift)
+import Utils.Vars
 import Require.Core.Term
 
-import Control.Monad.Cont
 import Control.Monad.Reader
 import Control.Monad.State
 
@@ -44,7 +44,7 @@ f .:: g = \x y z -> f (g x y z)
 -- correspond to a function   @\c -> foo x * c x@. We can therefore represent
 -- this by using a reader.
 
-type Continuer a = Reader (Term -> Term) a
+type Continuer a = Reader (Scope TermF) a
 
 
 -- | This is the core of what makes a continuer go. Every constructor is
@@ -52,7 +52,8 @@ type Continuer a = Reader (Term -> Term) a
 -- constructing the basic @Continuer@.
 
 continue :: Term -> Continuer Term
-continue x = reader ($ x)
+continue x = do sc <- ask
+                return (instantiate sc [x])
 
 
 
@@ -80,27 +81,27 @@ makeContinuer (In x) = In <$> traverse (underF makeContinuer) x
 -- need to compose up a function that will pick the appropriate one from a
 -- list.
 
-newtype Shifter a = Shifter { runShifter :: State Int ([Term] -> a, [Term]) }
+newtype Shifter a = Shifter { runShifter :: State Int (a, [(String,Term)]) }
 
 
 -- | A shifter is evaluated by evaluating it's state starting with 0.
 
-evalShifter :: Shifter a -> ([Term] -> a, [Term])
+evalShifter :: Shifter a -> (a, [(String,Term)])
 evalShifter (Shifter x) = evalState x 0
 
 
 instance Functor Shifter where
   fmap f x = Shifter $ do
-               (g,e) <- runShifter x
-               return (f.g, e)
+               (x',nes) <- runShifter x
+               return (f x', nes)
 
 
 instance Applicative Shifter where
-  pure x = Shifter (pure (const x, []))
+  pure x = Shifter (pure (x, []))
   f <*> x = Shifter $ do
-              (f',e) <- runShifter f
-              (x',e') <- runShifter x
-              return (\ms -> f' ms (x' ms), e ++ e')
+              (f',nes) <- runShifter f
+              (x',nes') <- runShifter x
+              return (f' x', nes ++ nes')
 
 
 
@@ -115,7 +116,8 @@ shift :: Term -> Shifter Term
 shift x = Shifter $ do
             i <- get
             put (i+1)
-            return ((!! i), [x]) 
+            let n = "auto_shift_" ++ show i
+            return (Var (Free (FreeVar n)), [(n,x)]) 
 
 
 
@@ -141,12 +143,11 @@ makeShifter (In x) = In <$> traverse (underF makeShifter) x
 
 reset :: Term -> Term
 reset x
-  | null shifts = m []
-  | otherwise =
-    let ks = mapM (cont . runReader . makeContinuer) shifts
-    in reset (runCont ks m)
+  | null shifts = m
+  | otherwise   = reset (foldr abstractor m shifts)
   where
     (m,shifts) = evalShifter (makeShifter x)
+    abstractor (n,x') m' = runReader (makeContinuer x') (scope [n] m')
 
 
 
